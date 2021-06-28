31d660e60e5cdfd95449cea03d55c6b2125832f1
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-06-28 15:21:24 -0300

    Adicionado textView editText e algumas strings.

--- app/src/main/java/com/netoneze/easytaskmanager/MainActivity.java
+++ app/src/main/java/com/netoneze/easytaskmanager/MainActivity.java
@@ -1,14 +1,51 @@
 package com.netoneze.easytaskmanager;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextTitulo, editTextData, editTextLocal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextTitulo = findViewById(R.id.editTextTitulo);
+        editTextData = findViewById(R.id.editTextData);
+        editTextLocal = findViewById(R.id.editTextOnde);
     }
+
+    public void limparCampos(View view){
+        editTextTitulo.setText(null);
+        editTextData.setText(null);
+        editTextLocal.setText(null);
+
+        editTextTitulo.requestFocus();
+
+        Toast.makeText(this,
+                R.string.campos_limpos,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void mostrarTitulo (View view){
+        String titulo = editTextTitulo.getText().toString();
+
+        if (titulo == null || titulo.trim().isEmpty()){
+            Toast.makeText(this,
+                    R.string.erro_campo_vazio,
+                    Toast.LENGTH_SHORT).show();
+            editTextTitulo.requestFocus();
+
+            return;
+        }
+
+        Toast.makeText(this, titulo.trim(), Toast.LENGTH_SHORT).show();
+    }
+
 }
