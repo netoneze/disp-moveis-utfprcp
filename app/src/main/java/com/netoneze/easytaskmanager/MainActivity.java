db95c449912acad839429d2a640becc93d82833d
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-06-29 16:25:11 -0300

    Adicionado RadioGroup com 3 RadioButtons.

--- app/src/main/java/com/netoneze/easytaskmanager/MainActivity.java
+++ app/src/main/java/com/netoneze/easytaskmanager/MainActivity.java
@@ -1,9 +1,11 @@
 package com.netoneze.easytaskmanager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText editTextTitulo, editTextData, editTextLocal, editTextDescricao;
     private CheckBox cbDiaInteiro;
+    private RadioGroup radioGroupPeriodo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         editTextLocal = findViewById(R.id.editTextOnde);
         editTextDescricao = findViewById(R.id.editTextDescricao);
         cbDiaInteiro = findViewById(R.id.checkBoxDiaInteiro);
+        radioGroupPeriodo = findViewById(R.id.radioGroupPeriodo);
     }
 
     public void limparCampos(View view){
@@ -32,22 +36,40 @@ public class MainActivity extends AppCompatActivity {
         editTextDescricao.setText(null);
         cbDiaInteiro.setChecked(false);
         editTextTitulo.requestFocus();
-
+        radioGroupPeriodo.clearCheck();
         Toast.makeText(this,
                 R.string.campos_limpos,
                 Toast.LENGTH_SHORT).show();
     }
 
+    @SuppressLint("NonConstantResourceId")
     public void mostrarTitulo (View view){
         String titulo = editTextTitulo.getText().toString();
 
-        if (titulo == null || titulo.trim().isEmpty()){
+        if (titulo == null || titulo.trim().isEmpty() || radioGroupPeriodo.getCheckedRadioButtonId() == -1){
             Toast.makeText(this,
                     R.string.erro_campo_vazio,
                     Toast.LENGTH_SHORT).show();
             editTextTitulo.requestFocus();
 
             return;
+        }
+
+        switch(radioGroupPeriodo.getCheckedRadioButtonId()){
+            case R.id.radioButtonPeriodoManha:
+                Toast.makeText(this, "Periodo manhã selecionado", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.radioButtonPeriodoTarde:
+                Toast.makeText(this, "Periodo tarde selecionado", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id .radioButtonPeriodoNoite:
+                Toast.makeText(this, "Periodo noite selecionado", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+
         }
 
         Toast.makeText(this, titulo.trim(), Toast.LENGTH_SHORT).show();
