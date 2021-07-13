39dd44032c70b7b5685aa9375347604bf6060d4e
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-07-13 14:48:05 -0300

    Adicionado view de autoria do app

---
+++ app/src/main/java/com/netoneze/easytaskmanager/ActivityAutoriaView.java
@@ -0,0 +1,14 @@
+package com.netoneze.easytaskmanager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityAutoriaView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autoria_view);
+    }
+}
