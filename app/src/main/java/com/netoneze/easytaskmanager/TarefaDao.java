524c7c343cb6fd4d9a8f1a6bbc66703330e4a2ef
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-03 10:52:26 -0300

    Criado DAO da entidade Tarefa usando Room

---
+++ app/src/main/java/com/netoneze/easytaskmanager/TarefaDao.java
@@ -0,0 +1,27 @@
+package com.netoneze.easytaskmanager;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TarefaDao {
+    @Insert
+    long insert(Tarefa tarefa);
+
+    @Delete
+    void delete(Tarefa tarefa);
+
+    @Update
+    void update(Tarefa tarefa);
+
+    @Query("SELECT * FROM tarefa WHERE id = :id")
+    Tarefa queryForId(long id);
+
+    @Query("SELECT * FROM tarefa ORDER BY id ASC")
+    List<Tarefa> queryAll();
+}
