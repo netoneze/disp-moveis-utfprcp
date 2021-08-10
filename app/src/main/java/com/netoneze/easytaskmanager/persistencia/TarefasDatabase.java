95a931363eb8483e3d458654c396f17abb809ab9
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-10 11:26:08 -0300

    Adicionado segunda entidade na notação do Room e atualizado versão do bd

--- app/src/main/java/com/netoneze/easytaskmanager/persistencia/TarefasDatabase.java
+++ app/src/main/java/com/netoneze/easytaskmanager/persistencia/TarefasDatabase.java
@@ -6,9 +6,10 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.netoneze.easytaskmanager.modelo.Disciplina;
 import com.netoneze.easytaskmanager.modelo.Tarefa;
 
-@Database(entities = {Tarefa.class}, version = 1, exportSchema = false)
+@Database(entities = {Tarefa.class, Disciplina.class}, version = 2, exportSchema = false)
 public abstract class TarefasDatabase extends RoomDatabase {
     public abstract TarefaDao tarefaDao();
 
