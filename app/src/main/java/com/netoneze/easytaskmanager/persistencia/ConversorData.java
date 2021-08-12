9f0ff3d7e56ea21d0dbc20153e12c13ca299efe3
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-12 16:42:56 -0300

    Adicionado classe para conversão de data com TypeConverter

---
+++ app/src/main/java/com/netoneze/easytaskmanager/persistencia/ConversorData.java
@@ -0,0 +1,19 @@
+package com.netoneze.easytaskmanager.persistencia;
+
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+public class ConversorData {
+
+    @TypeConverter
+    public static Date fromTimestamp(Long value) {
+        return value == null ? null : new Date(value);
+    }
+
+    @TypeConverter
+    public static Long dateToTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+
+}
